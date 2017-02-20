8349b615f94b509df6d7d3dace41947d44d106a9
damnedx <damnedx>
2017-02-21 00:19:09 +0100

    little reorganisation of code

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-var http     = require("http");
-var allocine = require('allocine-api');
-var search   = require('./Utils/search.js');
-
-var server = http.createServer(function(request, response) {
-
-  function parseResult(elements)
-  {
-    console.log('parseResult');
-    var parsed;
-    elements.movie.forEach(movie => {
-      console.log(movie);
-      // var json = {
-      //   originalTitle: movie.originalTitle,
-      //   title        : movie.title,
-      //   releaseDate  : search.getData(movie, "release.releaseDate");
-      // }
-    });
-    // var movie = search.getData(elements, 'movie');
-
-    // return jsonObject({Films : data->films, Actors : data->castingShorts});
-  }
+var Http     = require("http");
+var Allocine = require('allocine-api');
+var Parser   = require('./Utils/parsing.js');
+var Server   = Http.createServer(function(request, response) {
 
   function insertData(db,data)
   {
     // db.Insert(result);
   }
 
-
   function processFilmData(film)
   {
     var result;
-    allocine.api('search', {q: film, filter: 'movie'}, function(error, results) {
+    Allocine.api('search', {q: film, filter: 'movie'}, function(error, results) {
       if(error) { console.log('Error : '+ error); return; }
-      
-      result = results.feed;
 
-      var parsed;
-      parsed = parseResult(result);
+      result = results.feed;
+      var parsed = Parser.parseResult(result);
       insertData('db1',result)
       insertData('db2', parsed);
 
@@ -49,5 +28,5 @@ var server = http.createServer(function(request, response) {
 
 });
 
-server.listen(8080);
+Server.listen(8080);
 console.log("Server is listening");
