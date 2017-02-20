f77d7948d1d783b4c7bd6c22e3528ee2c591fc44
Hamza SBITI <hamzasbiti@gmail.com>
2017-02-20 23:07:35 +0100

    Create Utils + search.js

---
+++ Utils/search.js
@@ -0,0 +1,30 @@
+var search = function() {
+	this.recursiveVariableSearch = function (data, vars) {
+        // Catching PHP flavor :-°
+        if (data == null) {
+            data = undefined;
+        }
+
+        if (data == undefined || vars.length == 0) {
+            return data;
+        }
+        
+
+        var needle = vars[0];
+
+        return recursiveVariableSearch(data[needle], vars.slice(1));
+    }
+
+    // Seeks a data in a "tree" object
+    // Ex: bpm.data(usecase, 'feed.movie', 'no')
+    this.data (data, search, notFoundValue) {
+        var tree  = search.split('.');
+        var found = recursiveVariableSearch(data, tree);
+
+        if (found == undefined) {
+            return notFoundValue;
+        }
+
+        return found;
+    }
+}
