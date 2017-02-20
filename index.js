d9e3e3603eb7b43d3286d48d630eacea5675a186
Hamza SBITI <hamzasbiti@gmail.com>
2017-02-20 22:13:29 +0100

    Add recursive object tree fetching function

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ var allocine = require('allocine-api');
 
 var server = http.createServer(function(request, response) {
 
+    var recursiveVariableSearch = function (data, vars) {
+        // Catching PHP flavor :-°
+        if (data == null) {
+            data = undefined;
+        }
+
+        if (data == undefined || vars.length == 0) {
+            return data;
+        }
+
+        var needle = vars[0];
+
+        return recursiveVariableSearch(data[needle], vars.slice(1));
+    }
+
+    // Seeks a data in a "tree" object
+    // Ex: bpm.data(usecase, 'feed.movie', 'no')
+    data = function (data, search, notFoundValue) {
+        var tree  = search.split('.');
+        var found = recursiveVariableSearch(data, tree);
+
+        if (found == undefined) {
+            return notFoundValue;
+        }
+
+        return found;
+    }
+
   function parseResult(data)
   {
     console.log('parseResult');
