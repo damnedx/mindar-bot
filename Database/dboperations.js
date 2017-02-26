3cb20c8df033f43034a1d7bce413fa1afbefa204
damnedx <damnedx>
2017-02-26 19:37:08 +0100

    Add suffix to DbOperations logger

--- Database/dboperations.js
+++ Database/dboperations.js
@@ -1,14 +1,15 @@
 var mongoose = require ("mongoose");
 
 var url = "mongodb://localhost/Mindar-dev";
+var info_console = "[DATABASE]";
 mongoose.Promise = global.Promise;
 // connect to database (single instance for all the app)
 function dboperations() {
   mongoose.connect(url, function (err, res) {
     if (err) {
-      console.log ('ERROR connecting to: ' + url + '. ' + err);
+      console.log (info_console + 'ERROR connecting to: ' + url + '. ' + err);
     } else {
-      console.log ('Succeeded connected to: ' + url);
+      console.log (info_console + 'Succeeded connected to: ' + url);
     }
   });
 
@@ -17,9 +18,9 @@ function dboperations() {
 dboperations.prototype.insertMovie = function(movie) {
     movie.save(function (err) {
       if (err)
-        console.log ('Error insert : ' + err)
+        console.log (info_console + 'Error insert : ' + err)
       else {
-        console.log('Inserted movie : ' + movie);
+        console.log(info_console + 'Inserted movie : ' + movie);
       }
     });
 }
