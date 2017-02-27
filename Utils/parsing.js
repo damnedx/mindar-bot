5ed202213eb897d2a096efa59ee83779961295d2
Hamza SBITI <hamzasbiti@gmail.com>
2017-02-27 01:08:52 +0100

    Add uniqueArray function

--- Utils/parsing.js
+++ Utils/parsing.js
@@ -2,23 +2,43 @@ var Search   = require('./search.js');
 var parsing = function() {
 
   this.parseMovie = function (elements){
+    var result = array();
 
-    // elements.movie.forEach(movie => {
-    //    var item = {
-    //      originalTitle: movie.originalTitle,
-    //      title        : movie.title,
-    //      releaseDate  : Search.getObjectData(movie, "release.releaseDate")
-    //    };
-    //    console.log(item);
-    // });
+    elements.movie.forEach(movie => {
+       var movieRes = {
+        title: {
+          originalTitle : movie.originalTitle,
+          usualTitle    : movie.title,
+          code          : movie.code,
+        },
+        actors: {names: movie.castingShort.actors}
+       };
+       // Ici faire la requete 2 pour récupérer les acteurs avec leur code.
 
-    return elements;
+       result.push(movieRes);
+    });
+
+    return result;
   }
 
   this.parsePerson = function (elements) {
     return elements;
   }
 
+  this.uniqueArray = function(a) {
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+         var item = a[i];
+         if(seen[item] !== 1) {
+               seen[item] = 1;
+               out[j++] = item;
+         }
+    }
+    return out;
+  }
 
   return this;
 
