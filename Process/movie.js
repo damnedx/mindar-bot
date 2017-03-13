efbbe79dde85ee1a43f5db86ccffe45018a0d295
Hamza SBITI <hamzasbiti@gmail.com>
2017-03-13 20:11:29 +0100

    Check if movie in database before inserting

--- Process/movie.js
+++ Process/movie.js
@@ -13,61 +13,58 @@ var movie = function() {
 
     this.store = function (name) {
       Search.searchData('movie', name).then(res => {
-        var counter = 0;
-        for (var i = 0; i < res.movie.length; i++) {
-          ///////// Uncomment me to show bug
-          // DBOperations.exists(collectionProd, {code: res.movie[i].code}).then( (cnt) => {
-            // if (cnt == 0) {
-              // console.log(i); // Problem here, takes always i = res.movie.length
-              this.insert(res.movie[i].code)
-            // }
-          // });
-        }
+        res.movie.forEach(movie => {
+          DBOperations.exists(collectionProd, {code: movie.code}).then( (cnt) => {
+            if (cnt == 0) {
+              this.insert(movie.code)
+            } else {
+              console.error(info_console + " code : " + movie.code + " already in database. Skip inserting");
+            }
+          });
+        });
       }).catch(function(e) {
         console.error(info_console +  'Promise error ' + e);
       });
     }
 
-        this.insert = function(movieCode) {
-          Search.getMovie(movieCode, "person").then(res => {
-            var data = JSON.stringify(res);
-            data = data.replace(/"\$":/g, '"type":');
-            data = JSON.parse(data);
-            
-            var dataProd = {
-              movie : {
-                code           : data.movie.code,
-                movieType      : data.movie.movieType,
-                originalTitle  : data.movie.originalTitle,
-                title          : data.movie.title,
-                keywords       : data.movie.keywords,
-                productionYear : data.movie.productionYear,
-                castMember     : data.movie.castMember,
-              } 
-            };
-
-
-            DBOperations.insertMovie(collectionProd, dataProd);
-            DBOperations.insertMovie(collectionArchive, data);
-
-          }).catch(function(e) {
-            
-          });
-    }
-
-    var getAllMoviesFromCodes = function(movieCodes){
-      var MovieModel = new Array();
-      var counter = 0;
-      var promiseArray = new Array();
-      for (var i = 0; i < movieCodes.length; i++) {
-        promiseArray.push(Search.getMovie(movieCodes[i], "person"));
-      }
-      Promise.all(promiseArray).then(movie => {
-        var data = JSON.stringify(movie);
+    this.insert = function(movieCode) {
+      Search.getMovie(movieCode, "person").then(res => {
+        var data = JSON.stringify(res.movie);
         data = data.replace(/"\$":/g, '"type":');
-        DBOperations.insert(collection, JSON.parse(data));
+        data = JSON.parse(data);
+        
+        var dataProd = {
+            code           : data.code,
+            movieType      : data.movieType,
+            originalTitle  : data.originalTitle,
+            title          : data.title,
+            keywords       : data.keywords,
+            productionYear : data.productionYear,
+            castMember     : data.castMember,
+        };
+
+
+        DBOperations.insertMovie(collectionProd, dataProd);
+        DBOperations.insertMovie(collectionArchive, data);
+
+      }).catch(function(e) {
+        
       });
     }
+
+    // var getAllMoviesFromCodes = function(movieCodes){
+    //   var MovieModel = new Array();
+    //   var counter = 0;
+    //   var promiseArray = new Array();
+    //   for (var i = 0; i < movieCodes.length; i++) {
+    //     promiseArray.push(Search.getMovie(movieCodes[i], "person"));
+    //   }
+    //   Promise.all(promiseArray).then(movie => {
+    //     var data = JSON.stringify(movie);
+    //     data = data.replace(/"\$":/g, '"type":');
+    //     DBOperations.insert(collection, JSON.parse(data));
+    //   });
+    // }
 
 	return this;
 }
